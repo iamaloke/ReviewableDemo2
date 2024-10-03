905a8c5941c697fb36509313ab54c6453526c269
Alok Kumar <alok.kumar@deltatre.com>
2024-10-03 18:20:31 +0530

    initial commit

---
+++ ReviewableDemo2/ReviewableDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewableDemo2App.swift
+//  ReviewableDemo2
+//
+//  Created by Alok Kumar on 03/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ReviewableDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
